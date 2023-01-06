bdf9627865a88a6782837cee7d928d90e4a300f1
marci <kismarczirobi@gmail.com>
2023-01-06 13:03:06 +0100

    Navigation, input and output decorator.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 recipes: Recipe[] = [
   new Recipe(
     "Food",
@@ -19,4 +20,8 @@ recipes: Recipe[] = [
       "https://www.allrecipes.com/thmb/pl6IzWa0p5VGZP-8ZsF4wfpEIwk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/8000900-2000-d41f8a550fe5444894bf4a9e4d84fd1c.jpg"
       )
 ];
+
+onRecipeSelected(recipe: Recipe){
+this.recipeWasSelected.emit(recipe);
+}
 }
